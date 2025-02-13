ca4a46f2bd329178df291afa08a08b096b280b90
Germey664 <ilia2005.doronin@mail.ru>
2025-02-13 12:33:05 +0300

    Создал def parser_file_toml и calc_func. Добавил чтение из config.toml

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-def say_s():
-    n = int(input("Input n:"))
-    if n < 0:
-        print("Error")
-    else:
-        for i in range(n):
-            print('*' * (n-i))
+import math
+import tomllib
+import pathlib
 
 
-say_s()
+def calc_func(a, b, c, xmin, xmax, step):
+    for x in range(xmin, xmax, step):
+        y = a * math.sqrt(1 + (2 * x) / (math.e**(b*x) + c**2))
+        print(y)
+
+
+def parser_file_toml():
+    # Read units from file
+    with pathlib.Path("config.toml").open(mode="rb") as file:
+        base_units = tomllib.load(file)
+
+    units = {}
+    for unit, unit_info in base_units.items():
+        units[unit] = unit_info
+        for alias in unit_info:
+            units[alias] = unit_info[alias]
+    return units
+
+
+units = parser_file_toml()
+calc_func(units['a'], units['b'], units['c'], units['xmin'], units['xmax'], units['step'])
